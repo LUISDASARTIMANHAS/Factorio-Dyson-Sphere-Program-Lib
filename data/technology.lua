2ecad66d69c3fef79bc9d8325b0e960faed8a2c5
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-07-19 13:40:04 -0300

    updated

--- data/technology.lua
+++ data/technology.lua
@@ -1,5 +1,5 @@
 local tech = require("functions.createTechnology")
--- default create name 
+-- default create name
 -- tech-dyson-nameofTech
 
 data:extend(
@@ -10,7 +10,10 @@ data:extend(
                 {"automation-science-pack", 3},
                 {"logistic-science-pack", 3}
             },
-            {"automation"},
+            {
+                {"automation"}
+            },
+            nil,
             500,
             20
         )
@@ -23,40 +26,172 @@ data:extend(
         tech.createTechnology(
             "electromagnetism",
             {
-                {"magnetic-coil", 1},
+                {"magnetic-coil", 1}
             },
-            {"tech-dyson-sphere-program"},
+            {
+                {"tech-dyson-sphere-program"}
+            },
+            nil,
             10,
             20
         )
     }
 )
 
--- Electromagnetism
+-- Planetary Logistics System
 data:extend(
     {
         tech.createTechnology(
-            "electromagnetism",
+            "planetary-logistics-system",
             {
-                {"magnetic-coil", 1},
+                {"electromagnetic-matrix", 8},
+                {"energy-matrix", 4}
             },
-            {"tech-dyson-sphere-program"},
+            {
+                {"turbo-transport-belt"}
+            },
+            nil,
+            100,
+            20
+        )
+    }
+)
+
+-- Titanium_Smelting
+data:extend(
+    {
+        tech.createTechnology(
+            "titanium-smelting",
+            {
+                {"electromagnetic-matrix", 2},
+                {"energy-matrix", 2}
+            },
+            {
+                {"steel-processing"}
+            },
+            nil,
+            100,
+            20
+        )
+    }
+)
+
+-- High-Strength Titanium Alloy
+data:extend(
+    {
+        tech.createTechnology(
+            "high-strength-titanium-alloy",
+            {
+                {"electromagnetic-matrix", 80},
+                {"energy-matrix", 80},
+                {"structure-matrix", 8}
+            },
+            {
+                {"tech-dyson-titanium-smelting"}
+            },
+            nil,
             10,
             20
         )
     }
 )
 
--- Electromagnetism
+
+-- reinforced thruster
 data:extend(
     {
         tech.createTechnology(
-            "electromagnetism",
+            "reinforced-thruster",
             {
-                {"magnetic-coil", 1},
+                {"energy-matrix", 16}
             },
-            {"tech-dyson-sphere-program"},
+            {
+                {"tech-dyson-sphere-program"}
+            },
+            nil,
+            100,
+            20
+        )
+    }
+)
+
+-- Interstellar Logistics System
+data:extend(
+    {
+        tech.createTechnology(
+            "interstellar-logistics-system",
+            {
+                {"electromagnetic-matrix", 120},
+                {"energy-matrix", 120},
+                {"structure-matrix", 12}
+            },
+            {
+                {"tech-dyson-high-strength-titanium-alloy"},
+                {"tech-dyson-reinforced-thruster"},
+            },
+            nil,
             10,
+            20
+        )
+    }
+)
+
+-- Interstellar Power Transmission
+data:extend(
+    {
+        tech.createTechnology(
+            "interstellar-power-transmission",
+            {
+                {"electromagnetic-matrix", 120},
+                {"energy-matrix", 120},
+                {"structure-matrix", 12}
+            },
+            {
+                {"tech-dyson-sphere-program"}
+            },
+            nil,
+            100,
+            20
+        )
+    }
+)
+
+-- Gas Giants Exploitation
+data:extend(
+    {
+        tech.createTechnology(
+            "gas-giants-exploitation",
+            {
+                {"electromagnetic-matrix", 12},
+                {"energy-matrix", 12},
+                {"structure-matrix", 12}
+            },
+            {
+                {"tech-dyson-interstellar-logistics-system"},
+                {"tech-dyson-interstellar-power-transmission"},
+            },
+            nil,
+            100,
+            20
+        )
+    }
+)
+
+
+-- High-Efficiency Plasma Control 
+data:extend(
+    {
+        tech.createTechnology(
+            "high-efficiency-plasma-control",
+            {
+                {"electromagnetic-matrix", 1},
+            },
+            {
+                {"tech-dyson-elelectromagnetism"},
+                {"fluid-handling"},
+            },
+            nil,
+            50,
             20
         )
     }
